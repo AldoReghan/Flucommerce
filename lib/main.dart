280cafe534e9975f435a1312aab2e63ef954b5e2
AldoReghan <aldoreghan2217@gmail.com>
2020-07-03 06:06:45 +0700

    flucommerce

--- lib/main.dart
+++ lib/main.dart
@@ -35,102 +35,105 @@ class _HomePageState extends State<HomePage> {
                   color: Colors.yellow,
                 ),
                 Positioned(
-                  top: 50,
-                  left: 20,
-                  right: 20,
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      Expanded(
-                        child: Icon(
-                          Icons.menu,
-                          color: Colors.black,
+                    top: 50,
+                    left: 20,
+                    right: 20,
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        Expanded(
+                          child: Icon(
+                            Icons.menu,
+                            color: Colors.black,
+                          ),
                         ),
-                      ),
-                      SizedBox(
-                        width: 50,
-                      ),
-                      Text(
-                        "Flucommerce",
-                        style: TextStyle(
-                            fontSize: 20, fontWeight: FontWeight.bold),
-                      ),
-                      SizedBox(
-                        width: 50,
-                      ),
-                      Expanded(
-                        child: Icon(
-                          Icons.shopping_cart,
-                          color: Colors.black,
+                        SizedBox(
+                          width: 50,
                         ),
-                      )
-                    ],
-                  )
-                ),
-                
+                        Text(
+                          "Flucommerce",
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
+                        ),
+                        SizedBox(
+                          width: 50,
+                        ),
+                        Expanded(
+                          child: Icon(
+                            Icons.shopping_cart,
+                            color: Colors.black,
+                          ),
+                        )
+                      ],
+                    )),
                 Positioned(
-                  top: 95,
-                  left: 10,
-                  right: 10,
-                  child: Container(
-                    height: 180,
-                    child: Swiper(
-                      itemBuilder: (context, index) {
-                        return Image.network(
-                            "http://via.placeholder.com/288x188");
-                      },
-                      itemCount: 3,
-                      viewportFraction: 0.8,
-                      scale: 0.9,
-                    ),
-                  )
-                ),
-                Positioned(
-                  bottom: -30,                                                                                                     
-                  left: 20,
-                  right: 20,
-                  child: Container(
-                    width: MediaQuery.of(context).size.width,
-                    height: 70,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: Colors.white,
-                      border: Border.all(color: Colors.black)
-                    ),                                                      
-                    child: Padding(
-                      padding: EdgeInsets.only(left: 20, right: 20, top: 15, bottom: 15),
-                      child: Row(
-                        children: <Widget>[
-                          Icon(Icons.search, color: Colors.blue),
-                          SizedBox(width: 15,),
-                          Text("Search...", style: TextStyle(
-                            fontSize: 18
-                          ),)
-                        ],
+                    top: 95,
+                    left: 10,
+                    right: 10,
+                    child: Container(
+                      height: 180,
+                      child: Swiper(
+                        itemBuilder: (context, index) {
+                          return Image.network(
+                              "http://via.placeholder.com/288x188");
+                        },
+                        itemCount: 3,
+                        viewportFraction: 0.8,
+                        scale: 0.9,
                       ),
-                    )
-                  )
-                )
-
+                    )),
+                Positioned(
+                    bottom: -30,
+                    left: 20,
+                    right: 20,
+                    child: Container(
+                        width: MediaQuery.of(context).size.width,
+                        height: 70,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: Colors.white,
+                            border: Border.all(color: Colors.black)),
+                        child: Padding(
+                          padding: EdgeInsets.only(
+                              left: 20, right: 20, top: 15, bottom: 15),
+                          child: Row(
+                            children: <Widget>[
+                              Icon(Icons.search, color: Colors.blue),
+                              SizedBox(
+                                width: 15,
+                              ),
+                              Text(
+                                "Search...",
+                                style: TextStyle(fontSize: 18),
+                              )
+                            ],
+                          ),
+                        )))
               ],
             ),
-            SizedBox(height: 50,),
+            SizedBox(
+              height: 50,
+            ),
             Padding(
               padding: EdgeInsets.only(left: 20, right: 20),
               child: Row(
                 children: <Widget>[
-                  Text("Categories", style: TextStyle(
-                    fontWeight: FontWeight.bold
-                  ),),
+                  Text(
+                    "Categories",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
                   Spacer(),
-                  Text("Show All", style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    color: Colors.blue
-                  ),)
+                  Text(
+                    "Show All",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold, color: Colors.blue),
+                  )
                 ],
               ),
             ),
-            SizedBox(height: 20,),
+            SizedBox(
+              height: 20,
+            ),
             Padding(
               padding: EdgeInsets.only(left: 20, right: 20),
               child: Row(
@@ -138,102 +141,124 @@ class _HomePageState extends State<HomePage> {
                 children: <Widget>[
                   Expanded(
                     child: Container(
-                      height: 50,
-                      decoration: BoxDecoration(
-                        border: Border.all(color: Colors.black),
-                        borderRadius: BorderRadius.circular(10)
-                      ),
-                      child: Center(
-                        child: Text("Assault")
-                      )
-                    ),
+                        height: 50,
+                        decoration: BoxDecoration(
+                            border: Border.all(color: Colors.black),
+                            borderRadius: BorderRadius.circular(10)),
+                        child: Center(child: Text("Assault"))),
                   ),
-                  SizedBox(width: 10,),
+                  SizedBox(
+                    width: 10,
+                  ),
                   Expanded(
                     child: Container(
-                      height: 50,
-                      decoration: BoxDecoration(
-                        border: Border.all(color: Colors.black),
-                        borderRadius: BorderRadius.circular(10)
-                      ),
-                      child: Center(
-                        child: Text("SMG")
-                      )
-                    ),
+                        height: 50,
+                        decoration: BoxDecoration(
+                            border: Border.all(color: Colors.black),
+                            borderRadius: BorderRadius.circular(10)),
+                        child: Center(child: Text("SMG"))),
                   ),
-                  SizedBox(width: 10,),
+                  SizedBox(
+                    width: 10,
+                  ),
                   Expanded(
                     child: Container(
-                      height: 50,
-                      decoration: BoxDecoration(
-                        border: Border.all(color: Colors.black),
-                        borderRadius: BorderRadius.circular(10)
-                      ),
-                      child: Center(
-                        child: Text("Pistol")
-                      )
-                    ),
+                        height: 50,
+                        decoration: BoxDecoration(
+                            border: Border.all(color: Colors.black),
+                            borderRadius: BorderRadius.circular(10)),
+                        child: Center(child: Text("Pistol"))),
                   ),
                 ],
               ),
             ),
-            SizedBox(height: 40,),
+            SizedBox(
+              height: 40,
+            ),
             Padding(
               padding: EdgeInsets.only(left: 20, right: 20),
               child: Row(
                 children: <Widget>[
-                  Text("Recommended", style: TextStyle(
-                    fontWeight: FontWeight.bold
-                  ),),
+                  Text(
+                    "Recommended",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
                   Spacer(),
-                  Text("Show All", style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    color: Colors.blue
-                  ),)
+                  Text(
+                    "Show All",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold, color: Colors.blue),
+                  )
                 ],
               ),
             ),
-            SizedBox(height: 20,),
+            SizedBox(
+              height: 20,
+            ),
             Padding(
-              padding: EdgeInsets.only(left: 20,right: 20),
+              padding: EdgeInsets.only(left: 20, right: 20),
               child: Container(
-                height: 80,
+                height: 250,
                 child: ListView(
                   scrollDirection: Axis.horizontal,
                   children: <Widget>[
-                    Row(
+                    Column(
                       children: <Widget>[
-                        Container(
-                          height: 90,
-                          width: 300,
-                          decoration: BoxDecoration(
-                            border: Border.all(color: Colors.black),
-                            borderRadius: BorderRadius.circular(10)
-                          ),
-                          child: Row(
-                            children: <Widget>[
-                              Container(
-                                height: MediaQuery.of(context).size.height,
-                                width: 60,
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.only(
-                                    topLeft: Radius.circular(9),
-                                    bottomLeft: Radius.circular(9)
+                        Card(
+                          borderOnForeground: true,
+                          shadowColor: Colors.black,
+                          child: Container(
+                            height: 220,
+                            width: 130,
+                            decoration: BoxDecoration(
+                                // border: Border.all(color: Colors.black),
+                                borderRadius: BorderRadius.circular(10)),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Container(
+                                  width: MediaQuery.of(context).size.width,
+                                  height: 100,
+                                  decoration: BoxDecoration(
+                                      color: Colors.red,
+                                      borderRadius: BorderRadius.only(
+                                          bottomRight: Radius.circular(20))),
+                                ),
+                                Container(
+                                  padding: EdgeInsets.all(10),
+                                  child: Text(
+                                    "Produk name",
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 16),
                                   ),
-                                  color: Colors.red,
                                 ),
-                                child: Text("data"),
-                              ),
-                              SizedBox(width: 10,),
-                              Container(
-                                child: Column(
-                                  children: <Widget>[
-                                    Text("data")
-                                  ],
+                                Container(
+                                  padding: EdgeInsets.only(left: 10, right: 10),
+                                  child: Text(
+                                    "Stok : 12",
+                                    style: TextStyle(fontSize: 14),
+                                  ),
                                 ),
-                              )
-                            ],
-                          )
+                                Container(
+                                  padding: EdgeInsets.only(
+                                      left: 10, right: 10, top: 5),
+                                  child: Text(
+                                    "Harga : 12.000",
+                                    style: TextStyle(fontSize: 14),
+                                  ),
+                                ),
+                                Container(
+                                  margin: EdgeInsets.only(top: 10),
+                                  height: 34,
+                                  decoration: BoxDecoration(
+                                      color: Colors.green,
+                                      borderRadius: BorderRadius.only(
+                                          topLeft: Radius.circular(20))),
+                                )
+                              ],
+                            ),
+                          ),
                         ),
                       ],
                     ),
@@ -241,7 +266,9 @@ class _HomePageState extends State<HomePage> {
                 ),
               ),
             ),
-            SizedBox(height: 20,)
+            SizedBox(
+              height: 10,
+            )
           ],
         ),
       ),
@@ -251,10 +278,22 @@ class _HomePageState extends State<HomePage> {
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: <Widget>[
-            Icon(Icons.home, color: Colors.grey,),
-            Icon(Icons.favorite, color: Colors.grey,),
-            Icon(Icons.compare_arrows, color: Colors.grey,),
-            Icon(Icons.person_pin, color: Colors.grey,),
+            Icon(
+              Icons.home,
+              color: Colors.grey,
+            ),
+            Icon(
+              Icons.favorite,
+              color: Colors.grey,
+            ),
+            Icon(
+              Icons.compare_arrows,
+              color: Colors.grey,
+            ),
+            Icon(
+              Icons.person_pin,
+              color: Colors.grey,
+            ),
           ],
         ),
       ),
