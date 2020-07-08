d332e91cd2c6dbf6d749d9c82214db39d0c7c822
AldoReghan <aldoreghan2217@gmail.com>
2020-07-08 22:54:51 +0700

    flucommerce

--- lib/login.dart
+++ lib/login.dart
@@ -20,19 +20,126 @@ class _LoginState extends State<Login> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
-        child: Stack(
-          alignment: Alignment.center,
-          overflow: Overflow.visible,
+        child: Column(
           children: <Widget>[
-            Container(
-              width: MediaQuery.of(context).size.width,
-              height: 300,
-              decoration: BoxDecoration(
-                color: Colors.blue,
+            Stack(
+              alignment: Alignment.center,
+              overflow: Overflow.visible,
+              children: <Widget>[
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: 250,
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                  ),
+                ),
+                Positioned(
+                  top: 70,
+                  left: 20,
+                  right: 20,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Icon(Icons.shopping_cart, color: Colors.white,),
+                      Text("Flucommerce", style: TextStyle(
+                        fontSize: 24,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold
+                      ),)
+                    ],
+                  )
+                ),
+                Positioned(
+                  bottom: -150,
+                  left: 10,
+                  right: 10,
+                  child: Container(
+                    height: 250,
+                    width: MediaQuery.of(context).size.width,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Colors.white,
+                      boxShadow: [
+                        BoxShadow(
+                          blurRadius: 7,
+                          color: Colors.grey,
+                          offset: Offset(0, 5)
+                        )
+                      ]
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.all(20.0),
+                          child: Text("LOGIN", style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            fontStyle: FontStyle.italic
+                          ),),
+                        ),
+                        Padding(padding: EdgeInsets.all(20),
+                          child: Row(
+                            children: <Widget>[
+                              Icon(Icons.person)
+                            ],
+                          ),
+                        ),
+                        Padding(padding: EdgeInsets.all(20),
+                          child: Row(
+                            children: <Widget>[
+                              Icon(Icons.lock)
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ))
+              ],
+            ),
+            SizedBox(height: 170,),
+            Padding(
+              padding: EdgeInsets.all(10),
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    height: 50,
+                    width: 200,
+                    decoration: BoxDecoration(
+                      color: Colors.green,
+                      borderRadius: BorderRadius.circular(20),
+                      boxShadow: [
+                        BoxShadow(
+                          blurRadius: 5,
+                          color: Colors.grey,
+                          offset: Offset(0, 5)
+                        )
+                      ]
+                    ),
+                    child: Center(
+                      child: Text("LOGIN", style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                        color: Colors.white
+                      ),),
+                    ),
+                  ),
+                  SizedBox(height: 20,),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text("Don't have account? "),
+                      Text("Register Here!", style: TextStyle(
+                        color: Colors.blue,
+                        fontStyle: FontStyle.italic,
+                      ),)
+                    ],
+                  )
+                ],
               ),
             )
           ],
-        ),
+        )
       ),
     );
   }
